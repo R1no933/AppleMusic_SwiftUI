951bf88aa722a5c9720000e26f10a2defb428db0
R1no933 <104028198+R1no933@users.noreply.github.com>
2022-09-14 14:39:35 +0300

    Initial commit

---
+++ AppleMusic_SwiftUI/AppleMusic_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic_SwiftUIApp.swift
+//  AppleMusic_SwiftUI
+//
+//  Created by Dmitriy Baskakov on 14.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMusic_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleMusic_SwiftUI
+//
+//  Created by Dmitriy Baskakov on 14.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
